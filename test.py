bb7499591037385c44f7a0613b90b6197a2d6108
seoulJoy <135812814+seoulJoy@users.noreply.github.com>
2024-06-02 21:15:14 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('hello world')
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://ridibooks.com/category/new-releases/2200'
+response = requests.get(url)
+response.encoding = 'utf-8'
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser') # html 파서 씀
+
+bookservices = soup.select('.title_text') # 클래스 짚어내기
+for no, book in enumerate(bookservices, 1):
+    print(no, book.text.strip())
